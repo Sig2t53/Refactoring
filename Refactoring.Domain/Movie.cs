1dad4791d44e077ff27c6d56e9a64a948a4bf963
Sig2t53 <seigo2t53@gmail.com>
2018-12-03 17:51:35 +0900

    MovieクラスからPriceクラスへGetChargeメソッドを移動した(※ビルドは通らない)

--- Refactoring.Domain/Movie.cs
+++ Refactoring.Domain/Movie.cs
@@ -54,32 +54,6 @@ namespace Refactoring.Domain
             PriceCode = priceCode;
         }
 
-        public double GetCharge(int dayRented)
-        {
-            double result = 0;
-            switch (PriceCode)
-            {
-                case REGULAR:
-                    result += 2;
-                    if (dayRented > 2)
-                    {
-                        result += (dayRented - 2) * 1.5;
-                    }
-                    break;
-                case NEW_RELEASE:
-                    result += dayRented * 3;
-                    break;
-                case CHILDRENS:
-                    result += 1.5;
-                    if (dayRented > 3)
-                    {
-                        result += (dayRented - 3) * 1.5;
-                    }
-                    break;
-            }
-            return result;
-        }
-
         public int GetFrequentRenterPoints(int dayRented)
         {
             //新作を２日以上かりた場合はボーナスポイント
--- Refactoring.Domain/Price.cs
+++ Refactoring.Domain/Price.cs
@@ -9,5 +9,31 @@ namespace Refactoring.Domain
     public abstract class Price
     {
         public abstract int GetPriceCode();
+
+        public double GetCharge(int dayRented)
+        {
+            double result = 0;
+            switch (PriceCode)
+            {
+                case REGULAR:
+                    result += 2;
+                    if (dayRented > 2)
+                    {
+                        result += (dayRented - 2) * 1.5;
+                    }
+                    break;
+                case NEW_RELEASE:
+                    result += dayRented * 3;
+                    break;
+                case CHILDRENS:
+                    result += 1.5;
+                    if (dayRented > 3)
+                    {
+                        result += (dayRented - 3) * 1.5;
+                    }
+                    break;
+            }
+            return result;
+        }
     }
 }
