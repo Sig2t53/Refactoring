1cca852aeddc5835f336047c5d32ff3f2745ed6e
Sig2t53 <seigo2t53@gmail.com>
2018-12-03 17:36:24 +0900

    State/Strategyパターンにでタイプコードを置き換えるため、料金を表すオブジェクトを追加

---
+++ Refactoring.Domain/RegularPrice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactoring.Domain
+{
+    public class RegularPrice : Price
+    {
+        public override int GetPriceCode()
+        {
+            return Movie.REGULAR;
+        }
+    }
+}
